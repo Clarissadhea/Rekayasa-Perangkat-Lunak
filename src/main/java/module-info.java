2b04c54f1dcf3688ddaa1bd5d24dff7282ffddd4
D <2410817220023@mhs.ulm.ac.id>
2026-04-27 17:56:26 +0800

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.classdiagramsimulation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.classdiagramsimulation to javafx.fxml;
-    exports com.example.classdiagramsimulation;
-}
